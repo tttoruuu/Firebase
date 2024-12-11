5b2682a40167cd44a438f0b8eda3bda6cbe11f2a
toru <toru09may@gmail.com>
2024-12-11 23:23:30 +0900

    4

--- training_page.py
+++ training_page.py
@@ -5,7 +5,7 @@ import openai
 import os
 
 # Streamlit SecretsからAPIキーを取得
-api_key = st.secrets["OPEN_API_KEY"]
+api_key = st.secrets["OPENAI_API_KEY"]
 client =OpenAI(api_key=api_key)
 
 def switch_page(page_name):
